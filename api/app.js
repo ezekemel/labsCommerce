613959c3454b6b95ff0d0b32f6d9198ff68398d5
Ezequiel <ezekemel@gmail.com>
2020-12-11 13:04:42 -0300

    Getting data from MeLi API with Axios

--- api/app.js
+++ api/app.js
@@ -12,14 +12,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/getiphone', (req,res)=>{
-  request('https://api.mercadolibre.com/sites/MLA/search?q=iphone', function (error, response, body) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-    res.send(body)
-  });
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- api/routes/search.js
+++ api/routes/search.js
@@ -1,9 +1,27 @@
 const express = require("express")
 let router = express.Router();
+const axios = require('axios').default;
+
 
 router.route('/')
-    .get((req,res)=>{
-        res.send('Hello Search!')
-    })
+    .get((req,res)=> {
+        const { query } = req.query;
+        axios.get("https://api.mercadolibre.com/sites/MLA/search?q=" + query)
+        .then(response => {            
+            const products = response.data.results.map((e) => {
+              return {
+                title: e.title,
+                price: e.price,
+                money: e.currency_id,               
+                condition: e.condition
+              }
+            });
+            res.send(products);
+          })
+          .catch(err => res.send(err));
+      });
+       
+         
+          
 
 module.exports = router; 
