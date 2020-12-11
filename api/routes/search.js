91f7745f695b7d52839d6d384923206f0e422b17
Ezequiel <ezekemel@gmail.com>
2020-12-11 08:33:24 -0300

    basic routing search

--- api/routes/search.js
+++ api/routes/search.js
@@ -1,6 +1,9 @@
 const express = require("express")
 let router = express.Router();
 
-
+router.route('/')
+    .get((req,res)=>{
+        res.send('Hello Search!')
+    })
 
 module.exports = router; 
