e8d85e79bf4bd6b9d66e335b56c12830caa58d7b
Ezequiel <ezekemel@gmail.com>
2020-12-11 09:33:59 -0300

    simple MeLi API request

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 const search = require('./routes/search')
+var request = require('request')
 
 app.use("/search", search);
 //use search.js file to handle endpoints 
@@ -9,6 +10,15 @@ app.use("/search", search);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/getiphone', (req,res)=>{
+  request('https://api.mercadolibre.com/sites/MLA/search?q=iphone', function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.send(body)
+  });
 })
 
 app.listen(port, () => {
